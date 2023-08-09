053808b8f27019b7bac69e660687bbe4afef60c5
gaddamrk <raj>
2023-08-09 18:49:35 +0530

    jr

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ module "subnets" {
   default_vpc_id = var.default_vpc_id
 
   vpc_id = module.vpc.vpc_id
+
   for_each = var.subnets
   cidr_block = each.value.cidr_block
   availability_zone = each.value.availability_zone
