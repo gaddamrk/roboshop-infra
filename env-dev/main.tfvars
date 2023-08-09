082c52c4cc910123c1a9c54db65667d246765839
gaddamrk <raj>
2023-08-09 19:27:48 +0530

    jr

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -2,21 +2,17 @@ env = "dev"
 default_vpc_id = "vpc-09bb62cc3d02287fb"
 
 vpc = {
+
   main = {
     cidr_block = "10.0.0.0/16"
   }
 }
 
-#public_subnets_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
-#private_subnets_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
-#availability_zones    = ["us-east-1a", "us-east-1b"]
-#}
-
-
 subnets = {
 
   public = {
     name                 = "public"
+    vpc_name             =  "main"
     cidr_block           = ["10.0.0.0/24", "10.0.1.0/24"]
     availability_zone    = ["us-east-1a", "us-east-1b"]
 
--- main.tf
+++ main.tf
@@ -9,19 +9,21 @@ module "vpc" {
 }
 
 
-#module "subnets" {
-#  source = "github.com/gaddamrk/tf-module-subnets"
-#  env = var.env
-#  default_vpc_id = var.default_vpc_id
-#
-#  vpc_id = module.vpc.vpc_id
-#
-#  for_each = var.subnets
-#  cidr_block = each.value.cidr_block
-#  availability_zone = each.value.availability_zone
-#  name = each.value.name
-#
-#}
+module "subnets" {
+  source = "github.com/gaddamrk/tf-module-subnets"
+  env = var.env
+  default_vpc_id = var.default_vpc_id
+
+
+
+  for_each            = var.subnets
+  cidr_block          = each.value.cidr_block
+  availability_zone   = each.value.availability_zone
+  name                = each.value.name
+  vpc_id              = lookup(module.vpc, each.value.vpc_name, null )
+
+
+}
 
 output "vpc_id" {
   value = module.vpc
