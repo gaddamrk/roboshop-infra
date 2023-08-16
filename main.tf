1f769bc8491e1086b7ba4c179086a2514b6b35ed
gaddamrk <raj>
2023-08-16 17:04:11 +0530

    jr

--- main.tf
+++ main.tf
@@ -5,35 +5,36 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value.cidr_block
-  public_subnet_ids = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  subnets    = each.value.subnets
+  availability_zone = each.value.availability_zone
 
 
 
 }
 
-
-module "subnets" {
-  source = "github.com/gaddamrk/tf-module-subnets"
-  env = var.env
-  default_vpc_id = var.default_vpc_id
-
-
-
-  for_each            = var.subnets
-  cidr_block          = each.value.cidr_block
-  availability_zone   = each.value.availability_zone
-  name                = each.value.name
-  vpc_id              = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null)
-  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_peering_connection_id", null)
-  internet_gw_id = lookup(lookup(module.vpc, each.value.vpc_name, null ), "internet_gw_id", null)
-
-  internet_gw = lookup(each.value, "internet_gw", false)
-  nat_gw = lookup(each.value, "nat_gw", false)
-
-
-}
-
-output "vpc_id" {
-  value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-}
-
+#
+#module "subnets" {
+#  source = "github.com/gaddamrk/tf-module-subnets"
+#  env = var.env
+#  default_vpc_id = var.default_vpc_id
+#
+#
+#
+#  for_each            = var.subnets
+#  cidr_block          = each.value.cidr_block
+#  availability_zone   = each.value.availability_zone
+#  name                = each.value.name
+#  vpc_id              = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_id", null)
+#  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null ), "vpc_peering_connection_id", null)
+##  internet_gw_id = lookup(lookup(module.vpc, each.value.vpc_name, null ), "internet_gw_id", null)
+#
+#  internet_gw = lookup(each.value, "internet_gw", false)
+#  nat_gw = lookup(each.value, "nat_gw", false)
+#
+#
+#}
+#
+#output "vpc_id" {
+#  value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#}
+#
