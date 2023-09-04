817c2e1f4de532c8dc825f18001c07109421b1a8
gaddamrk <raj>
2023-09-04 15:20:45 +0530

    jr

--- main.tf
+++ main.tf
@@ -73,18 +73,18 @@ module "rabbitmq" {
 
 }
 
-#module "alb" {
-#  source            = "github.com/gaddamrk/tf-module-alb"
-#  env               =  var.env
-#
-#  for_each          =  var.alb
-#  subnet_ids        =  lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
-#  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-#  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
-#  subnets_name      = each.value.subnets_name
-#
-#
-#}
-output "vpc" {
-  value = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnets", null), "public", null), "subnet_ids", null)
+module "alb" {
+  source            = "github.com/gaddamrk/tf-module-alb"
+  env               =  var.env
+
+  for_each          =  var.alb
+  subnet_ids        =  lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
+  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+  subnets_name      = each.value.subnets_name
+
+
 }
+#output "vpc" {
+#  value = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnets", null), "public", null), "subnet_ids", null)
+#}
